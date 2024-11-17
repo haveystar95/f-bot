c17b0b86d5994bbd5d173b5172608d9fb6bc19b1
denys.solonyna@yalantis.net <132371519+denys-yalantis@users.noreply.github.com>
2024-11-17 18:16:58 +0200

    update template

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Starting Docker container..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 docker run -d -p 80:8084 --name f-bot -v /var/param/.env:/app/.env $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/f-bot:latest
