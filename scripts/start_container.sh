4decaac3aff0ebb80ac60b23f1b4f4ae3757a0cc
denys.solonyna@yalantis.net <132371519+denys-yalantis@users.noreply.github.com>
2024-11-17 16:19:43 +0200

    update build v2

--- scripts/start_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting Docker container..."
-docker run -d -p 80:8084 --name f-bot -v /var/param/.env:/app/.env $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/f-bot:latest
--- scripts/stop_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Stopping any running Docker containers..."
-docker stop f-bot || true
-docker rm f-bot || true
